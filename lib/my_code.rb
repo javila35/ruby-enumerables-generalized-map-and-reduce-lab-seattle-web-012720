372daaf61d9d337cebc72e71538c91f4ab00cefb
Joseph Avila <josepharthuravila@gmail.com>
2020-01-12 01:33:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def reduce(source_array, starting_point=0)
   index = 0
   total = 0 
   while index < source_array.length
-    total += yield(source_arrayarray[index])
+    total += yield(source_array[index])
     index += 1
   end
   total
