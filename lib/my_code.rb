cdd5e6cc0912df01435e648294ca5fc570420421
Joseph Avila <josepharthuravila@gmail.com>
2020-01-12 01:50:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,7 +16,7 @@ def reduce(source_array, starting_point=nil)
     sum = source_array[0]
     i = 1 
   end
-  while i < source_array.legnth
+  while i < source_array.length
   sum = yield(sum, source_array[i])
   i += 1
   end 
