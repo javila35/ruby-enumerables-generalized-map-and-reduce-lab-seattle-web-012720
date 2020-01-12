446f49dc458c02c801a9d6154eb6caec0b054391
Joseph Avila <josepharthuravila@gmail.com>
2020-01-12 00:48:20 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -2,6 +2,6 @@ def make_sandwhich(element1, element2)
   yield("A #{element1} and #{element2} sandwhich")
 end 
 
-make_sandwhich("chicken", "mayonaise") do |innards|
-  puts "#{innards} on rye"
+make_sandwhich("chicken", "mayonaise") do |inside|
+  puts "#{inside} on rye"
 end 
