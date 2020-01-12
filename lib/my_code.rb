29a5895d6e48937729c2076d70425802f358f736
Joseph Avila <josepharthuravila@gmail.com>
2020-01-12 01:00:53 +0000

    Automatically backed up by Learn

---
+++ lib/my_code.rb
@@ -0,0 +1,12 @@
+def map(source_array)
+  index = 0 
+  array = []
+  while index < source_array.length 
+    yield
+    index += 1 
+  end
+end 
+
+map(source_array) {
+  |array| array << source_array* -1
+}
