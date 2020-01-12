7ebcec652ea35ac5cc936c9313d9cecdb6333d32
Joseph Avila <josepharthuravila@gmail.com>
2020-01-12 00:45:44 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,10 +1,9 @@
 def make_sandwhich(element1, element2)
   base = "A #{element1} and #{element2}"
   puts base
-  yield
-  base
+  yield(base)
 end 
 
 make_sandwhich("chicken", "mayonaise") do |innards|
-  puts "making some tasty stuff..."
+  puts "#{innards} on rye"
 end 
