5774d0d8fde5aac2d2c4927ea491b31184c28504
Joseph Avila <josepharthuravila@gmail.com>
2020-01-12 00:52:23 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,8 +1,12 @@
-def make_sandwhich(element1, element2)
-  yield("A #{element1} and #{element2} sandwhich")
-end 
-
-make_sandwhich("chicken", "mayonaise") {
-  |b| 
-  "#{b} on rye ."
-}
+def make_sandwich(element1, element2)
+  yield("A #{element1} and #{element2} sandwich")
+end
+ 
+# Wheat, sure!
+make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b} on wheat" }
+ 
+# Bueno!
+make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b} on tortilla" }
+ 
+# Lewis Carroll's Kitchen
+make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b.reverse} on #{"bread".reverse}" }
