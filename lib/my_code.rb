8d25c260610578219963db61b787a5e01cd603bd
Joseph Avila <josepharthuravila@gmail.com>
2020-01-12 01:19:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,9 +3,9 @@ def map(array)
   index = 0 
   new_array = []
   while index < array.length
-  binding.pry
     insert = yield
     new_array << insert
+    binding.pry
     index += 1 
   end
   new_array
