ddfeb61dd391044a175189b931f6ba9627150485
Joseph Avila <josepharthuravila@gmail.com>
2020-01-12 00:50:44 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -2,5 +2,7 @@ def make_sandwhich(element1, element2)
   yield("A #{element1} and #{element2} sandwhich")
 end 
 
-make_sandwhich("chicken", "mayonaise") {|b| "#{b.reverse} on #{"bread".reverse}."}
+make_sandwhich("chicken", "mayonaise") {
+  |b| 
+  "#{b.reverse} on #{"bread".reverse}."}
 end 
