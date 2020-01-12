29b0ef7447ef9a89a60ed53d81890045d32e25c9
Joseph Avila <josepharthuravila@gmail.com>
2020-01-12 01:28:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,8 +2,8 @@ def map(array)
   index = 0 
   new_array = []
   while index < array.length
-    method = new_array.push(array[index])
-    yield(method)
+    new_array.push(array[index])
+    yield
     index += 1 
   end
   new_array
